7afb0b97cffa144e307d2e55a486d01cb9d6d8a0
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-05-25 21:14:33 -0500

    Estructuras dinámicas: Listas tipo Cola}

---
+++ 02_ESTRUTURAS DINAMICAS/EJEMPLO/src/EJEMPLOS.java
@@ -0,0 +1,17 @@
+
+public class EJEMPLOS {
+
+	public EJEMPLOS() {
+		System.out.print("hola mundo");
+	}
+	
+	public void imprimir() {
+		System.out.print("hola familia");
+	}
+
+	public static void main(String[] args) {
+		EJEMPLOS ejemplo1=new EJEMPLOS();
+		ejemplo1.imprimir();
+	}
+
+}
